9c48905538db7dd5cef0dc9d5865ea805e18b39a
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-08 20:49:02 +0200

    Added required attribute to fields in TextContainer

--- TextContainer.cs
+++ TextContainer.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DTOs
 {
     public class TextContainer
     {
+        [Required]
         public string Subject { get; set; }
+
+        [Required]
         public string Text { get; set; }
     }
 }
