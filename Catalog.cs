9a0ba45a4924e07a6f2718d3ef722f560bff9768
shadowue@hotmail.com <shadowue@hotmail.com>
2020-05-08 20:41:59 +0200

    Initial commit

---
+++ Catalog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DTOs
+{
+    class Catalog
+    {
+        List<string> TextsList { get; set; }
+
+        public Catalog()
+        {
+            TextsList = new List<string>();
+        }
+    }
+}
---
+++ TextContainer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DTOs
+{
+    public class TextContainer
+    {
+        public string Subject { get; set; }
+        public string Text { get; set; }
+    }
+}
